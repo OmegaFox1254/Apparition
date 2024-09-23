93416cd9b11dc448b2bfeef245d64904867f28c8
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2024-09-22 18:29:29 -0800

    merger
    merges the owners and a collaborator's things to add
    1. diablo-like bars
    2. better temporary textures
    3. WIP atmosphere
    4. updated code
    5. optimizations
    6. more customization features

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,36 +1,57 @@
 extends CharacterBody2D
 
 
-var SPEED = 100.0
-const SPRINTSPEED = 150.0
-const WALKSPEED = 100.0
-const JUMP_VELOCITY = -400.0
+@export var HEALTH:int = 100;
+@export var FLASH:int = 100;
+@export_category("bars")
+@export var speedBar:TextureProgressBar;
+@export var flashlightBar:TextureProgressBar;
+@export var healthBar:TextureProgressBar;
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+@export_category("speed")
+@export var SPRINTSPEED:float = 150.0;
+@export var WALKSPEED:float = 100.0;
+@export var SPRINTAMMT:float = 100.0;
+
+var SPEED:float = 100.0;
+var SPRINTOUT:bool = false;
+const JUMP_VELOCITY:float = -400.0;
+
+
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	
-	if Input.is_action_pressed("Sprint"):
+	if SPRINTAMMT <= 0:
+		SPRINTOUT = true
+	if SPRINTAMMT >= 100:
+		SPRINTOUT = false
+	if Input.is_action_pressed("Sprint") && SPRINTOUT == false:
 		SPEED = SPRINTSPEED
+		SPRINTAMMT-=3
 	else:
 		SPEED = WALKSPEED
-	
-	# Add the gravity.
+		SPRINTAMMT+=float(SPRINTOUT)
+		
+	speedBar.value = SPRINTAMMT
+	flashlightBar.value = FLASH
+	healthBar.value = HEALTH
+
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-	#Get the input from WASD and arrows, then normalize the vector
-	var directionVector = Vector2(Input.get_axis("Left", "Right"), Input.get_axis("Up", "Down")).limit_length()
 	
+	var directionVector = Vector2(Input.get_axis("Left", "Right"), Input.get_axis("Up", "Down")).limit_length(4)
+	$Sprite2D.frame_coords = directionVector + Vector2(1, 1)
+	print($Sprite2D.frame_coords)
 	if directionVector.x:
-		#Apply movement on the X axis
+
 		velocity.x = directionVector.x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	if directionVector.y:
-		#Apply movement on the Y axis
+
 		velocity.y = directionVector.y * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
