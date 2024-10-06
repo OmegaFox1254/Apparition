0e046fd3f3ecb637e612c69eeee43c4a8726d003
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2024-10-05 21:35:05 -0800

    added smooth diming, astetic text, and mumble noises, as well as gave the ability to talk to the ghost multiple times

--- Scripts/dialogue.gd
+++ Scripts/dialogue.gd
@@ -4,36 +4,66 @@ extends Area2D
 @export var dialog:PackedStringArray
 @export var playerIsTalking:Array[bool]
 @export var currentPlayer:CharacterBody2D
-
+@export var deathAnimationTalk:AnimationPlayer
+@export var ghostAnimationTalk:AnimationPlayer
 
 var dialouging = false;
+var speaking = false;
 var currentText = 0;
+
+var currentTime:float = 0;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_body_entered(body: Node2D) -> void:
+	currentText = 0;
 	if body is player:
 		dialouging = true
 		textBox.visible = true;
-		text.text = dialog[currentText];
+		text.text = "";
+		for i in dialog[currentText]:
+			speaking = true;
+			text.text += i;
+			if(playerIsTalking[currentText]==true):
+				textBox.get_node("DeathPortrait").get_node("AudioStreamPlayer").playing = true;
+			else:
+				textBox.get_node("GhostPortrait").get_node("AudioStreamPlayer").playing = true;
+			await get_tree().create_timer(0.04).timeout;
+		speaking = false;
+		
+		if playerIsTalking[currentText] == true:
+			textBox.get_node("GhostPortrait").modulate = Color8(255, 255, 255, 73);
+			textBox.get_node("DeathPortrait").modulate = Color8(255, 255, 255, 255);
+		else:
+			textBox.get_node("GhostPortrait").modulate = Color8(255, 255, 255, 255);
+			textBox.get_node("DeathPortrait").modulate = Color8(255, 255, 255, 73);
+			
+			
 
-func _input(event) -> void:
+func _input(_event) -> void:
 	var Interacting = Input.is_action_just_pressed("Interact")
-	if Interacting and dialouging == true:
+	if Interacting and dialouging == true and speaking == false:
 		if currentText+1 < dialog.size():
 			currentText+=1;
-			text.text = dialog[currentText];
-			if playerIsTalking[currentText] == true:
-				textBox.get_node("GhostPortrait").modulate = Color8(255, 255, 255, 160)
-				textBox.get_node("DeathPortrait").modulate = Color8(255, 255, 255, 255)
-			else:
-				textBox.get_node("GhostPortrait").modulate = Color8(255, 255, 255, 255)
-				textBox.get_node("DeathPortrait").modulate = Color8(255, 255, 255, 160)
+			if playerIsTalking[currentText] == true && playerIsTalking[currentText-1]==false:
+				deathAnimationTalk.play_backwards("dim");
+				ghostAnimationTalk.play("dim");
+			elif playerIsTalking[currentText]==false && playerIsTalking[currentText-1]==true:
+				deathAnimationTalk.play("dim");
+				ghostAnimationTalk.play_backwards("dim");
 			
+			text.text = "";
+			for i in dialog[currentText]:
+				speaking = true;
+				text.text += i;
+				if(playerIsTalking[currentText]==true):
+					textBox.get_node("DeathPortrait").get_node("AudioStreamPlayer").playing = true;
+				else:
+					textBox.get_node("GhostPortrait").get_node("AudioStreamPlayer").playing = true;
+				await get_tree().create_timer(0.04).timeout;
+			speaking = false;
 		else:
 			dialouging = false;
 			textBox.visible = false;
-			$CollisionShape2D.disabled = true;
